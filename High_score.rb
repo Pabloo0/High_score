f91b06070f123c139a5433c793ad7a0cce6d16e9
Pabloo0 <kastelik.pawel@gmail.com>
2015-08-31 20:45:51 +0200

    Starting project - first working solution

--- High_score.rb
+++ High_score.rb
@@ -1,4 +1,4 @@
 require 'C:\Users\Pablo\RubymineProjects\support\High_score\process_file'
 ProcessFile.new do |line|
-  puts (line.split(" | "))
+  p (line.split(" | ")).map{ |player_scores| player_scores.split(" ").map(&:to_i)}.transpose.map(&:max).join(" ")
 end
