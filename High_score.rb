8c0d177c4d76fa5463b05a64fc11f1bd87e65bf8
Pabloo0 <kastelik.pawel@gmail.com>
2015-08-31 23:15:25 +0200

    Find high scores - better version

--- High_score.rb
+++ High_score.rb
@@ -1,4 +1,9 @@
 require 'C:\Users\Pablo\RubymineProjects\support\High_score\process_file'
 ProcessFile.new do |line|
-  p (line.split(" | ")).map{ |player_scores| player_scores.split(" ").map(&:to_i)}.transpose.map(&:max).join(" ")
+  p (line.
+        split(" | ")).
+        map{ |player_scores| player_scores.split(" ").map(&:to_i)}.
+        transpose.
+        map(&:max).
+        join(" ")
 end
