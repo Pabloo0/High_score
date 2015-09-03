21cca0874c7c9d64164a3753e0f369dafdf2605c
Pabloo0 <kastelik.pawel@gmail.com>
2015-09-03 21:21:33 +0200

    Find high scores - refractored

--- High_score.rb
+++ High_score.rb
@@ -1,9 +1,30 @@
+class HighScoreFinder < Struct.new(:line)
+  def max_score_per_category
+    scores_per_category.map(&:max)
+  end
+
+  def to_s
+    max_score_per_category.join(' ')
+  end
+
+  private
+
+  def scores_per_player_as_text
+    line.split(" | ")
+  end
+
+  def scores_per_player
+    scores_per_player_as_text.map do |player_scores|
+      player_scores.split(" ").map(&:to_i)
+    end
+  end
+
+  def scores_per_category
+    scores_per_player.transpose
+  end
+end
+
 require 'C:\Users\Pablo\RubymineProjects\support\High_score\process_file'
 ProcessFile.new do |line|
-  p (line.
-        split(" | ")).
-        map{ |player_scores| player_scores.split(" ").map(&:to_i)}.
-        transpose.
-        map(&:max).
-        join(" ")
+  puts HighScoreFinder.new(line)
 end
